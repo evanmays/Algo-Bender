ed4ab6afffff0ab2279d5778d35c8c7dfc9d9bf5
Evan Mays <me@evanmays.com>
2018-07-27 14:18:03 -0400

    first collection view commit

---
+++ Algo Bender/MyCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCollectionViewCell.swift
+//  Algo Bender
+//
+//  Created by Evan Mays on 7/27/18.
+//  Copyright © 2018 Evan Mays. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var value: UILabel!
+    @IBOutlet weak var position: UILabel!
+    @IBOutlet weak var valueWrapper: UIView!
+    
+    func configure(value: Int, position: Int) {
+        self.position.text = String(position)
+        self.value.text = String(value)
+        
+        valueWrapper.layer.cornerRadius = 10
+        valueWrapper.layer.borderWidth = 1.0
+        valueWrapper.layer.borderColor = UIColor.black.cgColor
+    }
+}
--- Algo Bender/SortingPracticeViewController.swift
+++ Algo Bender/SortingPracticeViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 import WebKit
 
-class SortingPracticeViewController: UIViewController {
+class SortingPracticeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
+    var items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]
+    
+    @IBOutlet var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,17 +26,21 @@ class SortingPracticeViewController: UIViewController {
     }
     
     deinit {
+        collectionView = nil
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "address", for: indexPath) as! MyCollectionViewCell
+        cell.configure(value: self.items[indexPath.item], position: indexPath.item)
+        return cell
+    }
+    
+    @IBAction func newNumbers(_ sender: UIButton) {
+        //reset items
+        //reload collection view
+    }
 }
