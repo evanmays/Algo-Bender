5e74648b419fcb9534314a0ff1bc24c79cec506c
Evan Mays <me@evanmays.com>
2018-07-27 13:40:41 -0400

    designed first iteration of sorting practice page

---
+++ Algo Bender/SortingPracticeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SortingPracticeViewController.swift
+//  Algo Bender
+//
+//  Created by Evan Mays on 7/27/18.
+//  Copyright © 2018 Evan Mays. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class SortingPracticeViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    deinit {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
