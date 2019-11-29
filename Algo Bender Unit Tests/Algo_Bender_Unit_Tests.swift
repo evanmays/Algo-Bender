50ec70eb42f474168e82dd5d86e334f6a2c8ca83
Evan Mays <me@evanmays.com>
2019-11-29 17:37:43 -0500

    Added Unit test target

---
+++ Algo Bender Unit Tests/Algo_Bender_Unit_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Algo_Bender_Unit_Tests.swift
+//  Algo Bender Unit Tests
+//
+//  Created by Evan Mays on 11/29/19.
+//  Copyright © 2019 Evan Mays. All rights reserved.
+//
+
+import XCTest
+
+class Algo_Bender_Unit_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
